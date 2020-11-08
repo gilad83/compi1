b90eb9e4a2938d185411edd4dcaf131f826bdd0d
darkn1k3 <secret678@gmail.com>
2020-11-08 16:27:22 +0200

    number related parsers

--- reader.ml
+++ reader.ml
@@ -3,7 +3,7 @@
 
 exception X_not_yet_implemented;;
 exception X_this_should_not_happen;;
-  
+
 type number =
   | Fraction of int * int
   | Float of float;;
@@ -27,9 +27,37 @@ let rec sexpr_eq s1 s2 =
   | String(s1), String(s2) -> s1 = s2
   | Symbol(s1), Symbol(s2) -> s1 = s2
   | Pair(car1, cdr1), Pair(car2, cdr2) -> (sexpr_eq car1 car2) && (sexpr_eq cdr1 cdr2);;
-  
+
+(************* our functions *************)
+let ascii_0 = 48;;
+
+let nt_whitespaces = PC.star PC.nt_whitespace;;
+
+let digit = PC.range '0' '9';;
+
+let digit_seq = PC.plus digit;;
+
+(* fold_left func [] [0,0,0,1,2,0] *)
+
+let zero = PC.const (fun ch -> ch = '0');;
+
+let zeros = PC.star zero;;
+
+let natural s = 
+  let (zrs, rest) = (zeros s) in
+  List.fold_left 
+    (PC.pack digit (fun ch -> (int_of_char ch) - ascii_0))
+    PC.nt_epsilon
+    rest;;
+
+(* let integer = caten sign natural_num;; *)
+
+let mantissa = digit_seq;;
+(************* end of our functions *************)
+
 module Reader: sig
   val read_sexprs : string -> sexpr list
+  val tests : unit -> unit
 end
 = struct
 let normalize_scheme_symbol str =
@@ -38,6 +66,9 @@ let normalize_scheme_symbol str =
 	(fun ch -> (ch = (lowercase_ascii ch)))
 	s) then str
   else Printf.sprintf "|%s|" str;;
+
+let tests () = 
+    assert((PC.test_string digit_seq "022a") = (['0';'2';'2'], "->[a]"));;
 
 
 let read_sexprs string = raise X_not_yet_implemented;;
